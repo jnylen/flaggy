305f33494f91c697cd0204f135a67f732a6a7a22
Joakim Nylen <git@joakim.nylen.nu>
2026-01-27 19:32:19 +0100

    fixes

--- build.sh
+++ build.sh
@@ -20,6 +20,7 @@ if [ -n "$AMO_API_KEY" ] && [ -n "$AMO_API_SECRET" ]; then
         --api-key "$AMO_API_KEY" \
         --api-secret "$AMO_API_SECRET" \
         --channel unlisted \
-        --artifacts-dir web-ext-artifacts
+        --artifacts-dir web-ext-artifacts \
+        --ignore-files build.sh .github web-ext-artifacts
     echo "Signed XPI available in web-ext-artifacts/"
 fi
